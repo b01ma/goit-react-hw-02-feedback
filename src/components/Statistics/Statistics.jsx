7e8ac7bfe10d12ff8d824edc10249e51fafcfded
ed_bolma <ed.bolma21@gmail.com>
2022-07-16 20:12:17 +0200

    added proptypes for Statistics

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,35 +1,42 @@
 // import css from './Statistics.module.css';
+import PropTypes from 'prop-types';
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
-    return (
-        <>
-            <h2>Statistics</h2>
+  return (
+    <>
+      <h2>Statistics</h2>
 
-            { total
-            ? (
-                <ul>
-                    <li>
-                    <p>Good: {good}</p>
-                    </li>
-                    <li>
-                    <p>Neutral: {neutral}</p>
-                    </li>
-                    <li>
-                    <p>Bad: {bad}</p>
-                    </li>
-                    <li>
-                    <p>Total: {total}</p>
-                    </li>
-                    <li>
-                    <p>Positive feedback: {positivePercentage}%</p>
-                    </li>
-                </ul>
-            ) : (
-                <p>No relusts</p> 
-            )
-            }
-        </>
-    )
+      {total ? (
+        <ul>
+          <li>
+            <p>Good: {good}</p>
+          </li>
+          <li>
+            <p>Neutral: {neutral}</p>
+          </li>
+          <li>
+            <p>Bad: {bad}</p>
+          </li>
+          <li>
+            <p>Total: {total}</p>
+          </li>
+          <li>
+            <p>Positive feedback: {positivePercentage}%</p>
+          </li>
+        </ul>
+      ) : (
+        <p>No relusts</p>
+      )}
+    </>
+  );
+};
+
+Statistics.propTypes = {
+  good: PropTypes.number,
+  neutral: PropTypes.number,
+  bad: PropTypes.number,
+  total: PropTypes.number,
+  positivePercentage: PropTypes.string,
 };
 
 export default Statistics;
